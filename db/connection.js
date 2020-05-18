78983577310af82303c6421ce263065f8af4af8d
ttdesign <tkthedesigner@gmail.com>
2020-05-18 07:40:39 -0700

    fixe connection js file

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,14 @@ mongoose.Promise = Promise;
 const mongoURI = 'mongodb://localhost/crm';
 
 mongoose
-	.connect(mongoURI, { useNewUrlParser: true })
+	.connect(mongoURI, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+	})
 	.then((instance) =>
-		console.log(`Connected to db${instance.connection.name}`)
+		console.log(`Connected to db ${instance.connection.name}`)
 	)
 	.catch((error) => console.log('Connection failed:', error));
 
