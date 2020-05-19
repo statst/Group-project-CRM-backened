9003bf192f607812e1c5e5c9c95660327e3bb2d2
foolan <foolanbhosale5@gmail.com>
2020-05-19 10:05:53 -0400

    creating newbranch

--- controllers/clients.js
+++ controllers/clients.js
@@ -2,47 +2,45 @@ const express = require('express');
 const router = express.Router();
 const Client = require('../models/Client');
 
-const { handleRecordExists } = require('../middleware/custom_errors');
-const { requireToken } = require('../middleware/auth');
+const {handleRecordExists} = require('../middleware/custom_errors');
+const {requireToken} = require('../middleware/auth');
 
 //route to get all clients info
 router.get('/', requireToken, (req, res) => {
-	Client.find({})
-		.then((clients) => res.json(clients))
-		.catch((error) => console.log(error));
+    Client.find({}).then((clients) => res.json(clients)).catch((error) => console.log(error));
 });
 //route to get clients by email
 router.get('/:email', requireToken, (req, res) => {
-	Client.findOne({ email: req.params.email })
-		.then(handleRecordExists)
-		.then((client) => res.json(client))
-		.catch((error) => console.log(error));
+    Client.findOne({email: req.params.email})
+        .then(handleRecordExists)
+        .then((client) => res.json(client))
+        .catch((error) => console.log(error));
 });
 
 //route to create new client
 router.post('/', requireToken, (req, res) => {
-	const newClient = req.body;
-	Client.create(newClient)
-		.then((client) => {
-			res.json(client);
-		})
-		.catch((error) => console.log(error));
+    const newClient = req.body;
+    Client.create(newClient)
+        .then((client) => {
+            res.json(client);
+        })
+        .catch((error) => console.log(error));
 });
 
 //route to edit client information
 //search for email and edit
 router.put('/:email', requireToken, (req, res) => {
-	Client.findOneAndUpdate({ email: req.params.email }, req.body, { new: true })
-		.then((client) => res.json(client))
-		.catch((error) => console.log(error));
+    Client.findOneAndUpdate({email: req.params.email}, req.body, {new: true})
+        .then((client) => res.json(client))
+        .catch((error) => console.log(error));
 });
 
 //route to delete client
 //find client by email and delete
-router.delete('/:email',requireToken (req, res) => {
-	Client.findOneAndDelete({ email: req.params.email })
-		.then(handleRecordExists)
-		.then((client) => res.json())
-		.catch((error) => console.log(error));
+router.delete('/:email', requireToken, (req, res) => {
+    Client.findOneAndDelete({email: req.params.email})
+        .then(handleRecordExists)
+        .then((client) => res.json())
+        .catch((error) => console.log(error));
 });
 module.exports = router;
